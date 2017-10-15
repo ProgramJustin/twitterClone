5758371f2d330fa6e01ccdcdae8fc7d25ed2bf8b
Justin Gomez <programjustin@gmail.com>
2017-10-14 18:01:49 -0700

    add what port to listen to

--- server/src/index.js
+++ server/src/index.js
@@ -2,3 +2,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 // create instance of express server
 const app = express();
+
+// what port to listen to, if process.env.PORT is undefined then choose port 3000
+const PORT = process.env.PORT || 3000;
