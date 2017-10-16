637822cfdde099accb2df63c6e5155c2a33bc1bb
Justin Gomez <programjustin@gmail.com>
2017-10-16 13:00:12 -0700

    set up graphql

--- server/src/index.js
+++ server/src/index.js
@@ -1,23 +1,43 @@
 /* eslint-disable no-console */
 
 import express from 'express';
+import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+
 import bodyParser from 'body-parser';
+import { createServer } from 'http';
 // import db
 import './config/db';
+import constants from './config/constants';
+import typeDefs from './graphql/schema';
+import resolvers from './graphql/resolvers';
 // create instance of express server
 const app = express();
 
-// what port to listen to, if process.env.PORT is undefined then choose port 3000
-const PORT = process.env.PORT || 3000;
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+
 
 // add middlewear inside app
 app.use(bodyParser.json());
 
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: constants.GRAPHQL_PATH
+}));
+
+app.use(constants.GRAPHQL_PATH, graphqlExpress({
+  schema
+}));
+
+const graphQLServer = createServer(app);
 // listen to port, log status
-app.listen(PORT, err => {
+graphQLServer.listen(constants.PORT, err => {
   if (err) {
     console.error(err);
   } else {
-    console.log(`App listen to port: ${PORT}`);
+    console.log(`App listen to port: ${constants.PORT}`);
   }
 });
