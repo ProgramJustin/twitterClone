f62ffb05f75e714c590db6252226cc9673e17609
Justin Gomez <programjustin@gmail.com>
2017-10-16 14:45:07 -0700

    complete fetch individual tweet feature

--- server/src/graphql/resolvers/index.js
+++ server/src/graphql/resolvers/index.js
@@ -2,6 +2,7 @@ import TweetResolvers from './tweet-resolvers';
 // query the database to extract tweets
 export default {
   Query: {
+    getTweet: TweetResolvers.getTweet,
     getTweets: TweetResolvers.getTweets,
   },
 };
--- server/src/graphql/resolvers/tweet-resolvers.js
+++ server/src/graphql/resolvers/tweet-resolvers.js
@@ -1,6 +1,9 @@
 import Tweet from '../../models/Tweet';
 // a resolver is a function that returns your data
 export default {
+  // findById is method from mongoose
+  // _, { _id }) => Tweet.findById(_id) instead of findById(args.id), _id variable is created and is stored as Tweet.findById(_id) 
+  getTweet: (_, { _id }) => Tweet.findById(_id),
   getTweets: () => Tweet.find({}),
 };
 // find({}) finds everythings
